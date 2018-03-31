02fe29a7aa7df2a6260cdd7d0a5b51ae854109a2
anugerah <anugerahnooriman71@gmail.com>
2018-03-31 12:54:35 +0700

    mengisi parameter

--- aan.java
+++ aan.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package percobaan2;
 
-/**
- *
- * @author user
- */
 public class aan {
+    double tinggi;
+    double rendah;
     
+    public aan(){
+        tinggi=10;
+        rendah=4;
+    }
+    
+    void cetakInfo(){
+        System.out.println("===================");
+        System.out.println("tinggi : "+tinggi);
+        System.out.println("rendah :    "+rendah);
+        System.out.println("===================");
+     
 }
